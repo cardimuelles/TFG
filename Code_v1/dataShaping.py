a505ba553c8f71e54f722f3c6e974316285fea12
jGomezSanz <jesus.gomez02@estudiant.upf.edu>
2019-12-04 17:47:06 +0100

    Add files via upload

---
+++ Code_v1/dataShaping.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+"""datashaping loading in file at each batch, format [batchSize,numStep]"""
+import scipy.io as sio
+import numpy as np
+
+
+# take input matrix and return shuffled train/test input/input
+def splitShuffleData(matrix,num_step,trainTestRatio,maxSize,features):
+    matrixIn = matrix[:maxSize,0]
+    matrixOut = matrix[:maxSize,1]
+    trainSize = int(len(matrixIn)*trainTestRatio)
+    testSize = maxSize-trainSize
+    reshapedInput  = []
+    reshapedOutput = []
+    nbExample = len(matrixIn)-num_step
+    for i in range(nbExample):
+        temp_list = matrixIn[i:i+num_step]
+        temp_list_out = [matrixOut[i+num_step-1]]
+        reshapedInput.append(np.array(temp_list))
+        reshapedOutput.append(np.array(temp_list_out))
+    inputShuffled = np.float32(np.array(shuffleMatrix(reshapedInput)).reshape(maxSize-num_step,num_step,features))
+    outputShuffled = np.float32(np.array(shuffleMatrix(reshapedOutput)).reshape(maxSize-num_step,1,features))
+    trainInput = inputShuffled[:trainSize]
+    trainOutput = outputShuffled[:trainSize]
+    testInput = inputShuffled[trainSize:]
+    testOutput = outputShuffled[trainSize:]
+    return trainInput,trainOutput,testInput,testOutput
+
+def shapeData(matrix,num_step,maxSize,features):
+    matrixIn = matrix[:maxSize,0]
+    matrixOut = matrix[:maxSize,1]
+    reshapedInput  = []
+    reshapedOutput =[]
+    nbExample = len(matrixIn)-num_step
+    for i in range(nbExample):
+        temp_list = matrixIn[i:i+num_step]
+        temp_list_out = [matrixOut[i+num_step-1]]
+        reshapedInput.append(np.array(temp_list))
+        reshapedOutput.append(np.array(temp_list_out))
+    reshapedInput = np.float32(np.array(reshapedInput).reshape(maxSize-num_step,num_step,features))
+    reshapedOutput = np.float32(np.array(reshapedOutput).reshape(maxSize-num_step,1,features))
+    return reshapedInput, reshapedOutput 
+
+def shuffleMatrix(data):
+    np.random.seed(42)
+    shuffled_indices = np.random.permutation(len(data))
+    return [data[i] for i in shuffled_indices]
+
+
+
